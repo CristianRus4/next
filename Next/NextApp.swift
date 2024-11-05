b5ab90135e9a712e160c5bef3080eb48905c71ce
Cristian Rus <cristianrus4@gmail.com>
2024-11-05 14:43:12 +1300

    Initial Commit

---
+++ Next/NextApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextApp.swift
+//  Next
+//
+//  Created by Cristian Rus on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
